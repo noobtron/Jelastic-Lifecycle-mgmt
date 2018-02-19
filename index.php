b302a51db9ea5650efb772c4570f11d6a691c820
noobtron <noobtron@gmail.com>
2018-02-19 15:29:22 +0200

    today fix

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ if (file_exists($filename)) {
     echo "The project was last modified: " . date("d.m.Y H:i:s.", filemtime($filename));
 }
 
-echo "Today = " date("d.m.Y H:i:s.");
+echo "Today = " . date("d.m.Y H:i:s.");
 echo date('l');
 echo "";
 ?>
