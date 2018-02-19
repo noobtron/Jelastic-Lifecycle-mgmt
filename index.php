f004845861041a51c923e282c2289978313da1fe
noobtron <noobtron@gmail.com>
2018-02-19 15:21:45 +0200

    modified date

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
-echo "Latest version<br>";
-echo date('w');
+$filename = 'index.php';
+if (file_exists($filename)) {
+    echo "The project was last modified: " . date ("F d Y H:i:s.", filemtime($filename));
+}
+
+echo "Latest version <br>";
+echo date('l');
 echo "";
 ?>
