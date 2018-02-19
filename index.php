cbeb7c5798fc9a7a04b9decda50d8ca6ba4ebb8b
noobtron <noobtron@gmail.com>
2018-02-19 15:36:50 +0200

    done

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $filename = 'index.php';
 if (file_exists($filename)) {
-    echo "The project was last modified: " . date("d.m.Y H:i:s.", filemtime($filename)) . "\n";
+    echo "The project was last updated: " . date("d.m.Y H:i:s.", filemtime($filename)) . "<br>";
 }
 echo "Today = " . date("d.m.Y H:i:s.");
 ?>
