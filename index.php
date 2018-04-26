253f51c9603b9cbfedd0f53c1bc150239ebf66ea
noobtron <noobtron@gmail.com>
2018-04-26 10:57:00 +0300

    muutos

--- index.php
+++ index.php
@@ -4,4 +4,5 @@ if (file_exists($filename)) {
     echo "The project was last updated: " . date("d.m.Y H:i:s.", filemtime($filename)) . "<br>";
 }
 echo "Today = " . date("d.m.Y H:i:s.");
+echo "muutos";
 ?>
