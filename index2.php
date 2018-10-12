0dc35e570484dcee6026dfb69d79e51b7e32ee71
noobtron <noobtron@gmail.com>
2018-10-12 09:49:04 +0300

    inedx2 remove

--- index2.php
+++
@@ -1,7 +0,0 @@
-<?php
-$filename = 'index.php';
-if (file_exists($filename)) {
-    echo "The project was last updated: " . date("d.m.Y H:i:s.", filemtime($filename)) . "<br>";
-}
-echo "Today = " . date("d.m.Y H:i:s.");
-?>
